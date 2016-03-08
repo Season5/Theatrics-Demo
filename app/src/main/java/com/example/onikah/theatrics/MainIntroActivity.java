badb0f0eab9907104c877e8d4813145fdca23cc5
Onikah <mona.kwamboka@gmail.com>
2016-03-08 14:00:08 +0300

    material intro

--- app/src/main/java/com/example/onikah/theatrics/MainIntroActivity.java
+++ app/src/main/java/com/example/onikah/theatrics/MainIntroActivity.java
@@ -1,13 +1,67 @@
 package com.example.onikah.theatrics;
 
+
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainIntroActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainIntroActivity  extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_intro);
+//        setContentView(R.layout.activity_main);
+
+        /**
+         * Standard slide (like Google's intros)
+         */
+        addSlide(new SimpleSlide.Builder()
+                .title(R.string.title_1)
+                .description(R.string.description_1)
+                .image(R.drawable.image_1)
+                .background(R.color.background_1)
+                .backgroundDark(R.color.background_dark_1)
+                .build());
+        /**
+         * Standard slide (like Google's intros)
+         */
+        addSlide(new SimpleSlide.Builder()
+                .title(R.string.title_2)
+                .description(R.string.description_2)
+                .image(R.drawable.image_2)
+                .background(R.color.background_2)
+                .backgroundDark(R.color.background_dark_2)
+                .build());
+
+
+
+
+        /* Enable/disable fullscreen */
+        setFullscreen(true);
+
+
+        /* Enable/disable skip button */
+        setSkipEnabled(true);
+
+        /* Enable/disable finish button */
+        setFinishEnabled(true);
+
+        /* Add your own page change listeners */
+        addOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+            @Override
+            public void onPageSelected(int position) {
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
     }
+
 }
